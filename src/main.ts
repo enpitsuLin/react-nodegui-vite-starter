26a6b3af28f2264ecaec38931aaa417dcdf77c2e
enpitsulin <enpitsulin@gmail.com>
2022-03-21 22:39:10 +0800

    for build and run

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from "@nodegui/vue-nodegui";
+import App from "./App.vue";
+
+const app = createApp(App);
+//@ts-ignore
+app.mount();
---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
+import { Plugin } from "vite";
+
+function nativePlugin(): Plugin {
+  return {
+    name: "native-plugin",
+    async transform(src, id) {
+      if (id.endsWith(".node")) {
+        return `
+            try {
+                global.process.dlopen(module, '${id}');
+            } catch(exception) {
+                throw new Error('Cannot open  ${id} :'+ exception);
+            }
+            export default module.exports;
+        `;
+      }
+    }
+  };
+}
+
+export default defineConfig({
+  plugins: [vue(), nativePlugin()],
+  build: {
+    outDir: "./dist",
+    sourcemap: false,
+    lib: {
+      entry: resolve(__dirname, "./src/main.ts"),
+      formats: ["cjs"],
+      fileName: () => "[name].cjs"
+    },
+    rollupOptions: {
+      external: ["@nodegui/nodegui"]
+    }
+  }
+});
